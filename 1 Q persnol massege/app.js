032fcb71d094db18d39d3a392ebc2cba4a750fc5
Muhammad Irfan <164501159+Muhammadirfan74@users.noreply.github.com>
2024-06-06 12:37:56 -0700

    Add files via upload

---
+++ 1 Q persnol massege/app.js
@@ -0,0 +1,2 @@
+var persnolName = "ERIC";
+console.log("hellow ".concat(persnolName, ",would you like   to learn some pythone today?"));
---
+++ 1 Q persnol massege/app.ts
@@ -0,0 +1,2 @@
+let persnolName: string= "ERIC";
+console.log(`hellow ${persnolName},would you like   to learn some pythone today?`);
